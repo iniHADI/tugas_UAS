f986ee248d0bf968236ce1a2e395d094e903372a
iniHADI <149175040+iniHADI@users.noreply.github.com>
2023-12-31 02:34:50 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
