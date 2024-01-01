d214773f6e7433038f3f555e0af471b94014b126
iniHADI <149175040+iniHADI@users.noreply.github.com>
2024-01-01 16:37:41 +0700

    Update ngetes.py

--- ngetes.py
+++ ngetes.py
@@ -91,7 +91,7 @@ def main():
 
     # Generate a sample dataset
     X, _ = make_blobs(n_samples=300, centers=4, cluster_std=1.0, random_state=42)
-    X = pd.DataFrame(X, columns=['CREDIT_LIMIT','PAYMENTS','TENURE'])
+    X = pd.DataFrame(X, columns=['PAYMENTS', 'CREDIT_LIMIT'])
     # Menampilkan dataset secara menyeluruh
     st.subheader("Dataset:")
     st.write(X)
@@ -100,7 +100,7 @@ def main():
     labels, cluster_centers = run_kmeans(n_clusters, X)
 
     # Menampilkan dataset asli dengan label cluster yang ditetapkan
-    original_data = pd.DataFrame(X, columns=['BALANCE','BALANCE_FREQUENCY','PURCHASES','INSTALLMENTS_PURCHASES','CASH_ADVANCE','PURCHASES_FREQUENCY','ONEOFF_PURCHASES_FREQUENCY','CASH_ADVANCE_FREQUENCY','CASH_ADVANCE_TRX','PURCHASES_TRX','CREDIT_LIMIT','PAYMENTS','MINIMUM_PAYMENTS','PRC_FULL_PAYMENT','TENURE'])
+    original_data = pd.DataFrame(X, columns=['PAYMENTS', 'CREDIT_LIMIT'])
     original_data['Cluster'] = labels
     st.subheader("Dataset Asli dengan Label Cluster:")
     st.write(original_data)
